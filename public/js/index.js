634f6169460d202871cd7aa7c96aef16fa77f516
jaehyanggg <jaehyanggg@google.com>
2021-05-14 12:30:19 +0900

    font 적용

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 /*************** 글로벌 설정 *****************/
 var auth = firebase.auth();	//firebase의 auth(인증)모듈을 불러온다.
 var googleAuth = new firebase.auth.GoogleAuthProvider();	// 구글로그인 모듈을 불러온다.
+var user = null;
 
 
 /*************** 사용자 함수 *****************/
@@ -8,10 +9,22 @@ var googleAuth = new firebase.auth.GoogleAuthProvider();	// 구글로그인 모�
 
 
 /*************** 이벤트 등록 *****************/
+auth.onAuthStateChanged(onChangeAuth);
+
 $('.bt-login').click(onLogin);
+$('.bt-logout').click(onLogOut);
 
 
 /*************** 이벤트 콜백 *****************/
+function onChangeAuth(r) {
+	user = r;
+	console.log(user);
+}
+
+function onLogOut() {
+	auth.signOut();
+}
+
 function onLogin() {
 	auth.signInWithPopup(googleAuth);
 }
